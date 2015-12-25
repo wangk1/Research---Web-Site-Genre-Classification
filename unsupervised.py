8a3ed566bddd85a0dc8da7d7214c977e7238afa6
wangk1 <kw1993oct@yahoo.com>
2015-12-25 14:40:30 -0500

    Added Adaboost.M1 with mnb

--- unsupervised.py
+++ unsupervised.py
@@ -25,11 +25,13 @@ def unsupervised(settings,train_set,clusterer,clustering_alg_cls):
         X,y,ref_ids=train_set.to_matrices()
 
         additional_notes=""
+        """
         if train_set.X.shape[0]<=settings.spectre_clustering_limit:
             clustering_alg=SpectralClustering(n_clusters=num_cluster)
             additional_notes="_spectral"
-        else:
-            clustering_alg=clustering_alg_cls(n_clusters=num_cluster)
+        else
+        """
+        clustering_alg=clustering_alg_cls(n_clusters=num_cluster)
 
         clustering_logger.info("Using {}".format(str(clustering_alg)+additional_notes))
 
@@ -126,7 +128,7 @@ if __name__=="__main__":
     settings=LearningSettings(type="unsupervised",dim_reduction="chi",feature_selection="summary",num_feats=10000)
     settings.parent_clusters=[] #used to record a tree of parent clusters for the current cluster
 
-    settings.clustering_alg="kNN_spectre"
+    settings.clustering_alg="kNN"
     clustering_alg=KMeans
     settings.num_clusters=list({10})
     settings.max_cluster_size=10000 #the cluster will be further broken up if it is greater than this size
