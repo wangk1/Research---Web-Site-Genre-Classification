5fb15dfb122ba172b95e546eac9bb5f6ee8f3299
wangk1 <kw1993oct@yahoo.com>
2016-01-10 11:18:31 -0500

    Convert Metadata and title to BOW

--- data.py
+++ data.py
@@ -1,10 +1,11 @@
 __author__ = 'Kevin'
 from data.db_to_pickle import db_to_pickle
-from db.db_model.mongo_websites_classification import URLAllGram
+from db.db_model.mongo_websites_classification import *
 from db.db_model.mongo_websites_models import URLBow
 
 
 
 if __name__=="__main__":
-    db_to_pickle(URLAllGram,"url")
+    db_to_pickle(WebpageMetaBOW,"metadata")
+    db_to_pickle(WebpageTitleBOW,"title")
     #db_to_pickle(URLBow,"summary")
