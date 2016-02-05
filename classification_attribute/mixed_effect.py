c5a86df9feb063385e6bf4caf8cb4a7502fb62c4
wangk1 <kw1993oct@yahoo.com>
2016-02-04 23:44:16 -0500

    Mixed Effect Prototype

--- classification_attribute/mixed_effect.py
+++ classification_attribute/mixed_effect.py
@@ -7,7 +7,7 @@ from sklearn.feature_selection import chi2,SelectKBest
 from util.Logger import Logger
 import numpy as np
 from db.db_model.mongo_websites_models import URLBow
-from scipy.sparse import coo_matrix
+from scipy.sparse import csc_matrix,lil_matrix,coo_matrix,vstack
 
 mixed_effect_logger=Logger(__name__)
 
@@ -17,60 +17,60 @@ Module to generate mixed effect X matrices.
 """
 
 
-def _mixed_effect_dict(vocab_vect,fitted_feature_selector):
-    """
-    Generate mapping of vocab -> index so we can map vocab pairs -> another index in the sparse X_mixed array
 
-    :param vocab_vect:
-    :param fitted_feature_selector:
+
+def _generate_mixed_effect_matrix(X_path,y_path,feat_selector):
+    """
+    Converts X to a COO Matrix of Mixed effect matrix
+
+    :param X_path:
+    :param y_path:
+    :param feat_selector:
     :return:
     """
-    vocab_selector=np.array(fitted_feature_selector.get_support())
 
-    #convert to object array because np does not like long strings
-    selected_vocab=np.array(vocab_vect.feature_names_,dtype=np.object)[vocab_selector].astype(np.str)
-
-    return {c:v for c,v in enumerate(selected_vocab)}
-
-
-def _generate_mixed_effect_matrix(X_path,y_path,vocab_vect_path,feat_selector,src_db_cls):
     mixed_effect_logger.debug("Flattening")
 
+    #Reduce the column count
     X,y,_=flatten_set(*random_pick_samples(unpickle_obj(X_path),genre_normalizer(unpickle_obj(y_path))))
     feat_selector.fit(X,y)
 
     mixed_effect_logger.debug("Final size of X: {} y:{}".format(X.shape,y.shape))
 
-    mixed_effect_dict=_mixed_effect_dict(unpickle_obj(vocab_vect_path),feat_selector)
+    #Get the column selector, indices
+    vocab_selector=feat_selector.get_support(True)
+    num_vocab=vocab_selector.shape[0]
 
-    X_mixed=coo_matrix((src_db_cls.objects.count(),len(mixed_effect_dict)**2))
-    ref_indexes=np.zeros((src_db_cls.objects.count()),dtype=np.int)
-    for ind,db_obj in enumerate(src_db_cls.objects.no_cache()):
-        ind % 10000==0 and mixed_effect_logger.info("Done with {}".format(ind))
-        attr_map=db_obj.attr_map
-        ref_indexes[ind]=db_obj.ref_index
+    vstack_list=[0]*X.shape[0]
+    for ind,X_row in enumerate(X):
+        ind % 10==0 and mixed_effect_logger.info("Done with {}".format(ind))
 
-        #identify words in the mixed effect dict
-        word_list_present=[w for w in attr_map.keys() if w in mixed_effect_dict]
+        row=np.zeros((1,num_vocab**2))
+        select_col=X_row[0,vocab_selector].toarray() #convert to dense rep.
 
-        for w1,w2 in itertools.product(word_list_present,word_list_present):
-            #index in array=index_w1*len(mixed_effect_dict)+index_w2, where index is index is word_list_present[w1|w2]
-            coo_index=word_list_present[w1]*len(mixed_effect_dict)+word_list_present[w2]
+        #Compare each index to each row. Record the minimum as cooccurence
+        for col_ind in range(0,select_col.shape[1]):
+            if not select_col[0,col_ind]:
+                continue
 
-            #cooccurence of w1,w2 is the minimum of the count b/w those two
-            X_mixed[ind,coo_index]=min(attr_map[w1],attr_map[w2])
+            cmp=np.full((1,select_col.shape[1]),fill_value=select_col[0,col_ind])
+            select_col=np.minimum(select_col,cmp)
+            row[0,col_ind*num_vocab:(col_ind+1)*num_vocab]=select_col
 
-    return X_mixed
+        vstack_list[ind]=lil_matrix(row)
+        del row,select_col
+
+    return vstack(vstack_list).tocoo()
 
 def mixed_effect_chisq():
-    k=30000
+    k=5000
     X_path="C:\\Users\\Kevin\\Desktop\\GitHub\\Research\\Webscraper\\pickle_dir\\summary\\X_summary_pickle"
     y_path="C:\\Users\\Kevin\\Desktop\\GitHub\\Research\\Webscraper\\pickle_dir\\summary\\y_summary_pickle"
     vocab_vect_path="C:\\Users\\Kevin\\Desktop\\GitHub\\Research\\Webscraper\\pickle_dir\\summary\\vocab_vectorizer_summary_pickle"
 
     feat_selector=SelectKBest(chi2,k)
 
-    X_mixed=_generate_mixed_effect_matrix(X_path,y_path,vocab_vect_path,feat_selector,URLBow)
+    X_mixed=_generate_mixed_effect_matrix(X_path,y_path,feat_selector)
 
 if __name__=="__main__":
     mixed_effect_chisq()
